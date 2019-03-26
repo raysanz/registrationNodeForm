46666bddb7b8727fbbc4aebd2ccd5b80ff6274b2
raysanz <rudysanchez93@gmail.com>
2019-03-25 20:13:22 -0700

    updated mongo credentials

--- public/routes/registration.routes.js
+++ public/routes/registration.routes.js
@@ -5,7 +5,6 @@ const registrationController = require('../controller/registration.controller')
 
 module.exports = router
 
-// ===========================================
 router.post('/', registrationController.insert)
 router.get('/', registrationController.getAll)
 router.get('/:id', registrationController.getOneById)
